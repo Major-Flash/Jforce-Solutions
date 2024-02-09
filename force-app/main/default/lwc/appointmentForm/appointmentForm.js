1b71860ed8256e2b1ae8ff8d0fc9ec5251c04ada
Majorflash <mominaman589@gmail.com>
2024-02-09 22:46:15 +0530

    initial commit

--- force-app/main/default/lwc/appointmentForm/appointmentForm.js
+++ force-app/main/default/lwc/appointmentForm/appointmentForm.js
@@ -1,6 +1,8 @@
 import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import checkAvailability from '@salesforce/apex/appointmentController.checkAvailability'
+import checkAvailability from '@salesforce/apex/appointmentController.checkAvailability';
+import isTimeExists from '@salesforce/apex/chechForDateandtimeDuplicate.isTimeExists';
+import handleDuplicateTimeslot from '@salesforce/apex/appointmentController.checkAvailability';
 
 export default class AppointmentForm extends LightningElement {
 
@@ -18,6 +20,36 @@ export default class AppointmentForm extends LightningElement {
         });
         this.dispatchEvent(evt);
     }
+    handleClick() {
+        handleDuplicateTimeslot()
+            .then(result => {
+                console.log('Success', result);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: 'The time slot is already Appointed',
+                        variant: 'error'
+                    })
+                );
+            })
+            .catch(error => {
+               console.log(error); // Handle error
+            });
+    }
+
+
+    
+    handleError() {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error',
+                message: 'An error occurred while saving the appointment',
+                variant: 'error'
+            })
+        );
+    }
+
+   
 
     customShowModalPopup(){
         this.customFormModal=true;
